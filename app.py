2906da856df2f7b99df3590e377f47e1843b25e5
Kenny Chavez <kennychaveez@gmail.com>
2021-10-29 12:35:30 -0600

    Added multiple status

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def login():
         cur = mysql.connection.cursor()
         cur.execute('SELECT * FROM tb_usuarios WHERE correo=%s and password=%s', (email, password))
         user = cur.fetchone()
-        print(user)
+
         if (user):
             session['user_id'] = user[0]
             session['user_name'] = user[1]
@@ -136,7 +136,8 @@ def save_history():
         cursor.execute('INSERT INTO tb_historial (id_tipo_incidencia, descripcion, id_proceso, fecha, id_usuario_creador, id_usuario_afectado, justificacion, estado, gravedad) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)', 
         (id_tipo_incidencia, descripcion, id_proceso, dt_string, id_usuario_creador, id_usuario_afectado, None, 'Creado', gravedad))
         mysql.connection.commit()
-        
+
+
         return redirect(url_for('get_created_incidences'))
 
 @app.route('/created-incidences')
@@ -149,12 +150,17 @@ def get_created_incidences():
 
 @app.route('/justification/<id>', methods=['GET'])
 def get_justification_form(id):
+    data = {}
     cursor = mysql.connection.cursor()
     cursor.execute("SELECT tbp.nombre as 'process_name', tbti.nombre as 'type', tbh.descripcion as 'description', tbu.nombre as 'user_name', tbh.fecha, tbh.gravedad, tbh.estado, tbh.id_historial, tbf.nombre as 'flow_name', tbh.justificacion FROM tb_historial tbh INNER JOIN tb_tipo_incidencia tbti ON tbh.id_tipo_incidencia = tbti.id_tipo_incidencia INNER JOIN tb_usuarios tbu ON tbh.id_usuario_creador = tbu.id_usuario INNER JOIN tb_procesos tbp ON tbh.id_proceso = tbp.id_proceso INNER JOIN tb_flujo tbf ON tbp.id_flujo = tbf.id_flujo WHERE id_historial = %s", [id])
-    history = cursor.fetchall()
+    data[0] = cursor.fetchone()
 
-    print(history)
-    return render_template('justification-form.html', history=history[0])
+    cursor.execute('SELECT * FROM tb_historial WHERE id_historial=%s', [id])
+    data[1] = cursor.fetchone()
+
+    cursor.execute('SELECT * FROM `tb_usuarios` WHERE id_area in (SELECT id_area FROM tb_procesos WHERE id_proceso = %s)', [data[1][3]])
+    data[2] = cursor.fetchall()
+    return render_template('justification-form.html', data=data)
 
 @app.route('/save-justificaton', methods=['POST'])
 def save_justification():
@@ -162,9 +168,30 @@ def save_justification():
         history_id = request.form['history_id']
         state = request.form['response']
         justification = request.form['justificacion']
-        cursor = mysql.connection.cursor()
-        cursor.execute("UPDATE tb_historial SET justificacion = %s, estado=%s WHERE id_historial = %s", [justification, state, history_id])
-        mysql.connection.commit()
+        
+        if state == 'Creado':
+            afected_user_id = request.form['id_usuario_afectado']
+            cursor = mysql.connection.cursor()
+            cursor.execute("UPDATE tb_historial SET justificacion = %s, estado=%s, id_usuario_afectado=%s WHERE id_historial = %s", [justification, state,afected_user_id, history_id])
+            mysql.connection.commit()
+        else: 
+            cursor = mysql.connection.cursor()
+            cursor.execute("UPDATE tb_historial SET justificacion = %s, estado=%s WHERE id_historial = %s", [justification, state, history_id])
+            mysql.connection.commit()
+
+        if state == 'Regresado': 
+            cursor.execute('SELECT * FROM tb_historial WHERE id_historial = %s', [history_id])
+            history = cursor.fetchone()
+            
+            now = datetime.now()
+            dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
+
+            cursor.execute('INSERT INTO tb_reasignacion (id_historial, fecha, id_usuario_creador, id_usuario_afectado, justificacion, estado) VALUES (%s, %s, %s, %s, %s, %s)', 
+            (history[0], dt_string, history[5], history[6], justification, state))
+            mysql.connection.commit()
+
+            cursor.execute('UPDATE tb_historial SET id_usuario_afectado=%s WHERE id_historial=%s', [history[5], history_id])
+            mysql.connection.commit()
 
         return redirect(url_for('get_my_incidences'))
 
